87fb1280394023c4fb0db2cd6e7d50d96d4c0161
Pankaj kumar singh <145023126+Pankaj9576@users.noreply.github.com>
2025-07-03 18:22:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -197,7 +197,7 @@ function App() {
         if (window.Swal) {
           window.Swal.fire({
             position: 'top-end',
-            icon: 'sweetalert2',
+            icon: 'success', // Corrected icon
             title: 'Registration Successful!',
             text: response.data.message,
             showConfirmButton: false,
@@ -215,21 +215,21 @@ function App() {
 
   return (
     <Container fluid className="form-wrapper" style={{ background: 'purple' }}>
+      {/* YouTube Iframe */}
+      <div className="youtube-iframe">
+        <iframe
+          width="200"
+          height="200"
+          src="https://www.youtube.com/embed/vp4O5W1gTjA"
+          title="YouTube video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
       <Row className="align-items-center min-vh-100 justify-content-center">
         <Col md={8} className="p-4">
           <div className="form-container">
-            {/* YouTube Iframe */}
-            <div className="youtube-iframe">
-              <iframe
-                width="200"
-                height="200"
-                src="https://www.youtube.com/embed/vp4O5W1gTjA"
-                title="YouTube video"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
             <Form onSubmit={handleSubmit}>
               <Row>
                 <Col md={6}>
